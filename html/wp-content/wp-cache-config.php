84e80dcbb7b0f430a86e1e2f2f0c0d6c7ef0c26e
zhangcunchaoOnlineLinux <zhangcunchao_cn@163.com>
2014-08-22 01:00:10 +0800

    everyday push

--- html/wp-content/wp-cache-config.php
+++ html/wp-content/wp-cache-config.php
@@ -13,11 +13,11 @@ $wp_cache_preload_email_volume = 'less'; //Added by WP-Cache Manager
 $wp_cache_preload_email_me = 1; //Added by WP-Cache Manager
 $wp_cache_preload_interval = 0; //Added by WP-Cache Manager
 $wp_cache_preload_posts = 'all'; //Added by WP-Cache Manager
-$cache_scheduled_time = '00:30'; //Added by WP-Cache Manager
+$cache_scheduled_time = '00:00'; //Added by WP-Cache Manager
 $wp_cache_mobile_groups = ''; //Added by WP-Cache Manager
 $wp_cache_mobile_prefixes = 'w3c , w3c-, acs-, alav, alca, amoi, audi, avan, benq, bird, blac, blaz, brew, cell, cldc, cmd-, dang, doco, eric, hipt, htc_, inno, ipaq, ipod, jigs, kddi, keji, leno, lg-c, lg-d, lg-g, lge-, lg/u, maui, maxo, midp, mits, mmef, mobi, mot-, moto, mwbp, nec-, newt, noki, palm, pana, pant, phil, play, port, prox, qwap, sage, sams, sany, sch-, sec-, send, seri, sgh-, shar, sie-, siem, smal, smar, sony, sph-, symb, t-mo, teli, tim-, tosh, tsm-, upg1, upsi, vk-v, voda, wap-, wapa, wapi, wapp, wapr, webc, winw, winw, xda , xda-'; //Added by WP-Cache Manager
-$wp_cache_refresh_single_only = '0'; //Added by WP-Cache Manager
-$wp_cache_make_known_anon = 0; //Added by WP-Cache Manager
+$wp_cache_refresh_single_only = '1'; //Added by WP-Cache Manager
+$wp_cache_make_known_anon = 1; //Added by WP-Cache Manager
 $wp_cache_mod_rewrite = 0; //Added by WP-Cache Manager
 $wp_cache_front_page_checks = 0; //Added by WP-Cache Manager
 $wp_cache_mfunc_enabled = 0; //Added by WP-Cache Manager
@@ -31,7 +31,7 @@ $wp_cache_slash_check = 1; //Added by WP-Cache Manager
 if ( ! defined('WPCACHEHOME') )
 	define( 'WPCACHEHOME', WP_CONTENT_DIR . "/plugins/wp-super-cache/" ); //Added by WP-Cache Manager
 
-$cache_compression = 0; // Super cache compression
+$cache_compression = 1; //Added by WP-Cache Manager
 $cache_enabled = true; //Added by WP-Cache Manager
 $super_cache_enabled = true; //Added by WP-Cache Manager
 $cache_max_time = 0; //Added by WP-Cache Manager
@@ -98,7 +98,7 @@ $wp_cache_pages[ "single" ] = 0;
 $wp_cache_pages[ "author" ] = 0;
 $wp_cache_hide_donation = 0;
 $wp_cache_not_logged_in = 0; //Added by WP-Cache Manager
-$wp_cache_clear_on_post_edit = 0; //Added by WP-Cache Manager
+$wp_cache_clear_on_post_edit = 1; //Added by WP-Cache Manager
 $wp_cache_hello_world = 0; //Added by WP-Cache Manager
 $wp_cache_mobile_enabled = 1; //Added by WP-Cache Manager
 $wp_cache_cron_check = 1; //Added by WP-Cache Manager
