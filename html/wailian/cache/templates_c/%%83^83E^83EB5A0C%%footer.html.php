c6726c5f68375aa0b937eb1d3a51ae2a3e59d1a5
zhangcunchaoOnlineLinux <zhangcunchao_cn@163.com>
2014-10-09 01:00:10 +0800

    everyday push-2014-10-09

--- html/wailian/cache/templates_c/%%83^83E^83EB5A0C%%footer.html.php
+++ html/wailian/cache/templates_c/%%83^83E^83EB5A0C%%footer.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.25, created on 2014-08-13 17:08:57
+<?php /* Smarty version 2.6.25, created on 2014-10-08 12:00:00
          compiled from www/footer.html */ ?>
 <div class="tags"><div>注意：本站从未表明收录成人站|垃圾站和其他一些不合法网站的意愿，此类站不收录。<br></div></div>
 <form action="/<?php echo $this->_tpl_vars['css']; ?>
@@ -14,11 +14,6 @@
 var cpro_id = "u1658531";
 </script>
 <script src="http://cpro.baidustatic.com/cpro/ui/f.js" type="text/javascript"></script>
-  <script type="text/javascript">
-    /*300*250 快照页创建于 2014-08-13*/
-    var cpro_id = "u1658559";
-</script>
-<script src="http://cpro.baidustatic.com/cpro/ui/s.js" type="text/javascript"></script>
 <script type="text/javascript" src="http://www.521php.com/api/tz/"></script>
 <div style="display:none">
 <script src="http://s96.cnzz.com/stat.php?id=4200165&web_id=4200165" language="JavaScript"></script>
