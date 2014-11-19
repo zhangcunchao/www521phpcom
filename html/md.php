664e2a95cd7a6aa9cce64e4e6e353d82ab3a787b
zhangcunchaoCaissa <zhangcunchao@caissa.com.cn>
2014-11-19 14:36:33 +0800

    update

--- html/md.php
+++ html/md.php
@@ -5,7 +5,9 @@
    header("Content-type: text/html; charset=utf-8");
    date_default_timezone_set('Asia/Shanghai');
    function markUrl($url){
+	   echo $url;
 	  $root = '/data/www/www.521php.com/html/';
+	  $url  = rtrim($url,'/');
 	  $url  = ltrim($url,$root);
 	  $d = explode('/',$url);
 	  $u = '';
