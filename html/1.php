fa2c432900aa489ea6e25be27cdb28837beb7645
zhangcunchaoOnlineLinux <zhangcunchao_cn@163.com>
2014-11-28 01:00:14 +0800

    everyday push-2014-11-28

--- html/1.php
+++ html/1.php
@@ -1,4 +1,6 @@
 <?php
+echo isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])?$_SERVER['HTTP_X_CLUSTER_CLIENT_IP']:$_SERVER['REMOTE_ADDR'];
+echo '<br />';
 echo $_SERVER['HTTP_USER_AGENT'].'<br />';
 echo $_SERVER['REQUEST_URI'];
 ?>
